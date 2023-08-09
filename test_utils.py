f5da64c96b93c2289c2cdca79bb5fc1c68e1cd75
anasbn44 <banouni.anas0@gmail.com>
2023-08-09 22:21:42 +0100

    commit-3

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_columns', None)
 
 import utils
@@ -7,15 +8,21 @@ import utils
 
 class Test(TestCase):
     def setUp(self) -> None:
-        self.ratings = pd.read_csv("dataset/ratings.csv")
-        self.movies = pd.read_csv("dataset/movies.csv")
-        self.genre_columns = ["Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary",
-                              "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi",
-                              "Thriller", "War", "IMAX", "Western", "(no genres listed)"]
+        self.ratings = pd.DataFrame(np.array([[1,1,4], [2,1,4.5], [2,2,2.5], [3,2,5]]),
+                                    columns=["userId","movieId","rating"], index=np.arange(1, 5))
+        self.movies = pd.DataFrame([[1,"Nixon (1995)","Drama"], [2,"Four Rooms (1995)","Comedy"]],
+                                   columns=["movieId","title","genres"], index=np.arange(1,3))
+        self.genre_columns = ["Comedy", "Drama"]
+        self.Y = pd.DataFrame(np.array([[4, 4.5, 0], [0, 2.5, 5]]),
+                              columns=np.arange(1, 4), index=np.arange(1, 3))
+        self.R = self.Y.mask(self.Y > 0, 1)
+        self.X = pd.DataFrame(np.array([[0, 1], [1, 0]]), columns=self.genre_columns, index=np.arange(1, 3))
 
     def test_prepare_data(self):
-        self.assertIsNotNone(utils.prepare_y_r(self.movies, self.ratings), "wrong")
+        Y, R = utils.prepare_y_r(self.movies, self.ratings)
+        print(Y.values)
+        self.assertEqual(Y.values, self.Y.values, "wrong")
 
     def test_prepare_x(self):
-        self.assertIsNotNone(utils.prepare_x(self.movies, self.genre_columns), "wrong")
+        self.assertEqual(utils.prepare_x(self.movies, self.genre_columns), self.X, "wrong")
 
